f906021f708a20887feb6877e517e6d448d31aa6
Emmanuel Douge <emmdouge@gmail.com>
2016-09-29 23:53:31 -0400

    AttackCommand extends AbstractAction for actionmap in button
    GUI is immediately redrawn for immediate response to input
    Properly use singleton
    refactored expressions into local boolean variables
    pulled methods into Environment
    Killing enemy immediately respawns them

--- src/command/AttackCommand.java
+++ src/command/AttackCommand.java
@@ -1,15 +1,20 @@
 package command;
 
-import lifeform.LifeForm;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import lifeform.Lifeform;
 import environment.Environment;
+import graphics.GUI;
 
 /**
  * @author Emmanuel
  * attacks enemies based on if the player has a weapon or not
  */
-public class AttackCommand implements Command {
+public class AttackCommand extends AbstractAction implements Command {
 	private Environment env;
-	private LifeForm lifeform;
+	private Lifeform lifeform;
 
 	/**
 	 * initializes the Environment instance variable
@@ -20,179 +25,56 @@ public class AttackCommand implements Command {
 		this(Environment.getInstance().getPlayer());
 	}
 	
-	public AttackCommand(LifeForm lifeform)
+	public AttackCommand(Lifeform lifeform)
 	{
-		this.env = Environment.getInstance(0, 0);
+		this.env = Environment.getInstance();
 		this.lifeform = lifeform;
-	}
-	
-	private LifeForm getLifeform() {
-		return this.lifeform;
 	}
 	
 	public void execute()
 	{
-		LifeForm opponent;
+		Lifeform opponent;
 		
 		//if the player has a weapon and wants to attack
-		if(getLifeform().hasWeapon() == true)
+		if(lifeform.hasWeapon())
 		{
 			//find the closest enemy
-			opponent = findClosest(getLifeform().getDirection());
+			opponent = env.getLifeformClosestTo(lifeform, lifeform.getDirection());
 			
 			//if an enemy is found, attack
-			if(opponent != null)
+			boolean enemyIsFound = opponent != null;
+			if(enemyIsFound)
 			{		
-				getLifeform().attack(opponent, env.computeRange(getLifeform(), opponent));
+				lifeform.attack(opponent, env.computeRange(lifeform, opponent));
 			}
 		}
 		//if the player does not have a weapon and wants to attack
 		else
 		{
 			//find the enemy that is front of the player in the direction he is facing
-			opponent = findInFrontOf(getLifeform().getDirection());
+			opponent = env.getLifeformInFrontOf(lifeform, lifeform.getDirection());
 			
 			//if an enemy is found, attack
-			if(opponent != null)
+			boolean enemyIsFound = opponent != null;
+			if(enemyIsFound)
 			{		
-				getLifeform().attack(opponent, env.computeRange(getLifeform(), opponent));
+				System.out.println("attack occurred");
+				lifeform.attack(opponent, 5);
 			}
+		}
+		
+		boolean opponentWasKilled = opponent != null && opponent.getCurrentLifePoints() <= 0;
+		if(opponentWasKilled)
+		{
+			RespawnCommand command = new RespawnCommand(opponent);
+			command.execute();
 		}
 	}
 
-	private LifeForm findInFrontOf(String direction) {
-		
-		if(direction == "north")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			if(i > 0)
-			{
-				i--;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}
-			
-		}
-		
-		else if(direction == "south")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			if(i < env.getRows())
-			{
-				i++;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}			
-			
-		}
-		
-		else if(direction == "east")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			if(j < env.getColumns())
-			{
-				j++;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}
-			
-		}	
-		
-		else if(direction == "west")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			if(j > 0)
-			{
-				j--;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}
-		}
-		
-		return null;
-	}
-	
-	private LifeForm findClosest(String direction) {
-
-		if(direction == "north")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			while(i > 0)
-			{
-				i--;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}
-			
-		}
-		
-		else if(direction == "south")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			while(i < env.getRows())
-			{
-				i++;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}			
-			
-		}
-		
-		else if(direction == "east")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			while(j < env.getColumns())
-			{
-				j++;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}
-			
-		}	
-		
-		else if(direction == "west")
-		{
-			int i = (int) env.findLifeForm(getLifeform()).getX();
-			int j = (int) env.findLifeForm(getLifeform()).getY();
-			
-			while(j > 0)
-			{
-				j--;
-				if(env.getLifeForm(i, j) != null)
-				{
-					return env.getLifeForm(i, j);
-				}	
-			}
-		}
-		
-		return null;
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		execute();
+		GUI.getInstance().redraw();
 	}
 }
